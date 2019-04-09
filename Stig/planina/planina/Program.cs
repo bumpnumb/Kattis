ed9165c275676b563ce869ac3128cce6f2be2333
Daniel Fennhagen <fennhagen.daniel@hotmail.com>
2019-04-09 17:42:09 +0200

    Final planina commit
    Includes comments

--- Stig/planina/planina/Program.cs
+++ Stig/planina/planina/Program.cs
@@ -10,6 +10,29 @@ namespace planina
     {
         static void Main(string[] args)
         {
+
+            // since 1sec cpu time we can do pretty much whatever with n max = 15
+            // looping through each iteration:
+            // every node in width and expanding with 1
+            // printing squared end width
+
+
+            int n = Int32.Parse(Console.ReadLine());
+            int width = 2;
+
+            for (int i = 0; i < n; i++)
+            {
+                int temp = 0;
+                for (int j = 1; j < width; j++)
+                {
+                    temp++;
+                }
+                width += temp;
+
+            }
+
+            Console.WriteLine(width* width);
+
         }
     }
 }
