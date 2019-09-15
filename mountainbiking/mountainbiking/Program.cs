70ec394a420c9c02b5ef4d47d08bd986e35aac11
Daniel Fennhagen <fennhagen.daniel@hotmail.com>
2019-09-15 14:35:35 +0200

    Initial mountainbiking commit

---
+++ mountainbiking/mountainbiking/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace mountainbiking
+{
+    //https://open.kattis.com/problems/mountainbiking
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
