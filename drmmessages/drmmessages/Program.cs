cddaa4818734dcbe06004ecdec65c4fa8b61ec54
Daniel Fennhagen <fennhagen.daniel@hotmail.com>
2019-09-17 11:14:31 +0100

    Create drmmessages

---
+++ drmmessages/drmmessages/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace drmmessages
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
