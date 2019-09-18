ce06fe6173ef74f676240b45a8a83b5c49faff97
Daniel Fennhagen <fennhagen.daniel@hotmail.com>
2019-09-18 09:14:59 +0100

    drmmessages attempt 2

--- drmmessages/drmmessages/Program.cs
+++ drmmessages/drmmessages/Program.cs
@@ -6,7 +6,17 @@ namespace drmmessages
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string text = Console.ReadLine();
+
+            string t1 = text.Substring(0, text.Length / 2 - 1);
+            string t2 = text.Substring(text.Length / 2 - 1, text.Length - 1);
+
+            foreach (char c in t1)
+            {
+
+            }
+
+
         }
     }
 }
