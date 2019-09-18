9cda699170a5ccdd7a94577f7519953120854252
Daniel Fennhagen <fennhagen.daniel@hotmail.com>
2019-09-18 09:50:32 +0100

    final drmmessages

--- drmmessages/drmmessages/Program.cs
+++ drmmessages/drmmessages/Program.cs
@@ -8,15 +8,38 @@ namespace drmmessages
         {
             string text = Console.ReadLine();
 
-            string t1 = text.Substring(0, text.Length / 2 - 1);
-            string t2 = text.Substring(text.Length / 2 - 1, text.Length - 1);
+            string t1 = text.Substring(0, text.Length / 2);
+            string t2 = text.Substring(text.Length / 2, text.Length / 2);
 
-            foreach (char c in t1)
+            int[] output = new int[text.Length / 2];
+
+
+            int rot1 = 0;
+            int rot2 = 0;
+            for (int i = 0; i < text.Length / 2; i++)
             {
-
+                rot1 += t1[i] - 'A';
+                rot2 += t2[i] - 'A';
             }
 
+            for (int i = 0; i < text.Length / 2; i++)
+            {
+                int a = t1[i] - 'A';
+                a += rot1;
+                a = a % ('Z' - 'A' + 1);
 
+                int b = t2[i] - 'A';
+                b += rot2;
+                b = b % ('Z' - 'A' + 1);
+
+                output[i] = (a + b) % ('Z' - 'A' + 1) + 'A';
+            }
+
+            foreach (int i in output)
+            {
+                Console.Write((char)i);
+            }
+            Console.Write("\r\n");
         }
     }
 }
